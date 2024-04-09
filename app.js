3c26144b0668461a9886a2a0bc32f280349ab2bc
gmmarsh <gmmarsh@gmail.com>
2024-04-09 11:53:05 -0400

    code re-organization

--- app.js
+++ app.js
@@ -1,43 +1,53 @@
-// Get the data
+// get the data once when the page loads
+let samples, metadata;
 d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
-    // extract the samples data
-    let samples = data.samples;
+    samples = data.samples;
+    metadata = data.metadata;
 
-
-// create the dropdown menu
-let dropdown = d3.select("#selDataset");
-
-    // append the Test Subject ID No. to the dropdown menu
-    samples.forEach((sample) => {
-        dropdown.append("option").text(sample.id).property("value", sample.id);
-    }
-    );
-
-    // display the data and the plots to the page
-    buildPlots(samples[0]);
-    buildMetadata(data.metadata[0]);
+    // populate the dropdown and display the initial data
+    populateDropdown(samples);
+    updateData(samples[0].id);
 });
 
 
-// function to handle the change of the dropdown menu
+// function to populate the dropdown
+function populateDropdown(samples) {
+    let dropdown = d3.select("#selDataset");
+    samples.forEach((sample) => {
+        dropdown.append("option").text(sample.id).property("value", sample.id);
+    });
+}
+
+// function to handle change in dropdown selection
 function optionChanged(id) {
-    d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
-        // extract the samples data
-        let samples = data.samples;
+    updateData(id);
+}
 
-        // filter the data for the selected ID
-        let sample = samples.filter(sample => sample.id == id)[0];
+// function to update the demographic info box with the selected ID
+function buildMetadata(metadata) {
+    let demographicInfo = d3.select("#sample-metadata");
 
-        // display the demographic info and the plots
-        buildPlots(sample);
-        buildMetadata(data.metadata.filter(sample => sample.id == id)[0]);
-    }
-    );
+    // clear the existing data
+    demographicInfo.html("");
+
+    // append the data to the panel
+    Object.entries(metadata).forEach(([key, value]) => {
+        demographicInfo.append("h6").text(`${key}: ${value}`);
+    });
+}
+
+// function to update the plots and metadata
+function updateData(id) {
+    let sample = samples.filter(sample => sample.id == id)[0];
+    let sampleMetadata = metadata.filter(meta => meta.id == id)[0];
+    buildPlots(sample);
+    buildMetadata(sampleMetadata);
 }
 
 
 // function to build the plots
 function buildPlots(sample) {
+
     // create the bar chart
     let barData = [
         {
@@ -53,9 +63,9 @@ function buildPlots(sample) {
         title: "Top 10 OTUs",
         margin: { t: 30, l: 150 }
     };
-
-    // plot the bar chart
+   
         Plotly.newPlot("bar", barData, barLayout);
+
 
     // create the bubble chart
     let bubbleData = [
@@ -78,19 +88,5 @@ function buildPlots(sample) {
         margin: { t: 30 }
     };
 
-    // plot the bubble chart
-    Plotly.newPlot("bubble", bubbleData, bubbleLayout);
-}
-
-// update the demographic info with the selected ID
-function buildMetadata(metadata) {
-    let demographicInfo = d3.select("#sample-metadata");
-
-    // clear the existing data
-    demographicInfo.html("");
-
-    // append the data to the panel
-    Object.entries(metadata).forEach(([key, value]) => {
-        demographicInfo.append("h6").text(`${key}: ${value}`);
-    });
+        Plotly.newPlot("bubble", bubbleData, bubbleLayout);
 }
